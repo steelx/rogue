78adcd935b356d9e3d5e72ffd1ae1745a489f9a4
Ajinkya Borade <ajinkyax@pm.me>
2026-05-22 11:23:22 +0530

    fix: ST Task Chase

---
+++ Source/Rogue/StateTree/ST_Cube/STTask_Chase.cpp
@@ -0,0 +1,26 @@
+﻿#include "STTask_Chase.h"
+
+#include "StateTreeExecutionContext.h"
+
+EStateTreeRunStatus FSTTask_Chase::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	if (!IsValid(InstanceData.OwningActor) || !IsValid(InstanceData.TargetPawn))
+	{
+		return EStateTreeRunStatus::Failed;
+	}
+
+	const FVector CurrentLocation = InstanceData.OwningActor->GetActorLocation();
+	const FVector TargetLocation = InstanceData.TargetPawn->GetActorLocation();
+
+	if (TargetLocation.Equals(CurrentLocation, InstanceData.InterpSpeed))
+	{
+		return EStateTreeRunStatus::Succeeded;
+	}
+
+	const FVector Location = FMath::VInterpTo(CurrentLocation, TargetLocation, DeltaTime, InstanceData.InterpSpeed);
+	InstanceData.OwningActor->SetActorLocation(Location);
+
+	return EStateTreeRunStatus::Running;
+}
---
+++ Source/Rogue/StateTree/ST_Cube/STTask_Chase.h
@@ -0,0 +1,40 @@
+﻿// Copyright 2026 Ajinkya Borade. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeTaskBase.h"
+#include "STTask_Chase.generated.h"
+
+
+USTRUCT()
+struct FST_ChaseInstanceData
+{
+	GENERATED_BODY()
+
+	/// CONTEXT
+	UPROPERTY(EditAnywhere, Category = "Context")
+	TObjectPtr<AActor> OwningActor;
+
+	/// INPUT
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<APawn> TargetPawn;
+
+	/// PARAMETER
+	UPROPERTY(EditAnywhere, Category = "Parameter")
+	float InterpSpeed {2.f};
+};
+
+USTRUCT(meta = (DisplayName = "Chase Target Pawn"))
+struct FSTTask_Chase: public FStateTreeTaskCommonBase
+{
+	GENERATED_BODY()
+
+	using FInstanceDataType = FST_ChaseInstanceData;
+	virtual const UStruct* GetInstanceDataType() const override
+	{
+		return FST_ChaseInstanceData::StaticStruct();
+	}
+
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+};
